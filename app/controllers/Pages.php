8a613d40c8278b5c7a8b33bb7b8952b5b6668cf8
AHMED RABIE MAHMOUD <ahmed.rabie.mahmoud.96@gmail.com>
2022-02-20 02:21:47 +0200

    Base Controller Class

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,9 +1,10 @@
 <?php
 
-class Pages {
+class Pages  extends Controller{
     public function __construct() {
     }
     public function index() {
+        $this->view('Hello');
     }
     public function about($id) {
         echo $id;
